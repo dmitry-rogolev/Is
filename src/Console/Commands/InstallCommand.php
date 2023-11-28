8452f7a0b0cce03ec571c2bd18e89167bedea668
dmitry-rogolev <work.drogolev@internet.ru>
2023-11-29 03:21:52 +0700

    v0.0.2

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -34,17 +34,20 @@ class InstallCommand extends Command
     public function handle()
     {
         $tag = 'is';
-        
+
         if ($this->option('config')) {
+            $this->call('slug:install', ['--config' => true]);
             $tag .= '-config';
         } else if ($this->option('migrations')) {
             $tag .= '-migrations';
         } else if ($this->option('seeders')) {
             $tag .= '-seeders';
-        } 
-        
+        } else {
+            $this->call('slug:install');
+        }
+
         $this->call('vendor:publish', [
-            '--tag' => $tag, 
+            '--tag' => $tag,
         ]);
     }
 }
